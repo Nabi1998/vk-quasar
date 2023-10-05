f56e400a44232cdd851fde960e69031ad0360e63
Nabi Sandurov <sandurovnabi98@gmail.com>
2023-10-05 22:04:00 +0600

    added navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="new-wrapper">
+         <q-layout view="lHh lpr lFf" container style="height: 50px" class="shadow-2 rounded-borders">
+      <q-header elevated>
+        <q-toolbar>
+          <q-toolbar-title>Quasar Framework</q-toolbar-title>
+
+          <q-btn flat round dense icon="whatshot" />
+          
+              <a href="#">Create</a>
+        
+        </q-toolbar>
+      </q-header>
+         </q-layout> 
+    </div>
+</template>
+
+
+<script>
+
+export default {
+  name: 'NavBar'
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    
+    <div>Ct</div>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
